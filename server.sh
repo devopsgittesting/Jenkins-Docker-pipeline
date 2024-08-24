93b90843c4ab85c2459d15e03a2a11b9145ffe34
PRAVIN <psidwadkar@gmail.com>
2024-08-24 21:54:21 +0530

    Docker file updated

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 echo "`$HOSTNAME`"
-#echo "<!DOCTYPE html> <html> <body>VM Instance Name: $HOSTNAME
+#echo "<!DOCTYPE html><html><body>VM Instance Name: $HOSTNAME
 #<p><strong>VM IP Address:<strong> $(hostname -i)</p></body></html>" | tee /usr/local/apache2/htdocs/index.html
-sleep 1000
+sleep 40
