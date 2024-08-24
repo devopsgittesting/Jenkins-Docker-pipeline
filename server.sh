4b47e37648d4879e6b194434e36baac07edbf193
PRAVIN <psidwadkar@gmail.com>
2024-08-24 21:00:54 +0530

    docker file updated

--- server.sh
+++ server.sh
@@ -2,3 +2,4 @@
 HOSTNAME=$(hostname)
 sudo echo "<!DOCTYPE html> <html> <body>VM Instance Name: $HOSTNAME
 <p><strong>VM IP Address:<strong> $(hostname -i)</p></body></html>" | sudo tee /usr/local/apache2/htdocs/
+sleep 1000
