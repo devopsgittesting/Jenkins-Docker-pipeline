187d49170881df36cbd647d77a4e0aa92fdd9dbb
PRAVIN <psidwadkar@gmail.com>
2024-08-24 21:46:57 +0530

    server.sh file updated

--- server.sh
+++ server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
-echo "<!DOCTYPE html> <html> <body>VM Instance Name: $HOSTNAME
-<p><strong>VM IP Address:<strong> $(hostname -i)</p></body></html>" | tee /usr/local/apache2/htdocs/index.html
+echo "`$HOSTNAME`"
+#echo "<!DOCTYPE html> <html> <body>VM Instance Name: $HOSTNAME
+#<p><strong>VM IP Address:<strong> $(hostname -i)</p></body></html>" | tee /usr/local/apache2/htdocs/index.html
 sleep 1000
