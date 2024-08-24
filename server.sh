3580a59a876b76e3afdc7e2ea9fe7e44f9697d4a
PRAVIN <psidwadkar@gmail.com>
2024-08-24 22:12:36 +0530

    server file updated

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
-echo "`$HOSTNAME`" | tee /usr/local/apache2/htdocs/index.html
+echo "HOSTNAME :$HOSTNAME VM IP Address: $(hostname -i)" | tee /usr/local/apache2/htdocs/index.html
 #echo "<!DOCTYPE html><html><body>VM Instance Name: $HOSTNAME
 #<p><strong>VM IP Address:<strong> $(hostname -i)</p></body></html>" | tee /usr/local/apache2/htdocs/index.html
 sleep 40
